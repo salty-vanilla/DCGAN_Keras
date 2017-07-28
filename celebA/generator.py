20554412c4be3591e0d1c4a048853c955f3ccb47
nakatsuka <jacketgardensio@gmail.com>
2017-07-28 11:22:11 +0900

    updete generator model

--- celebA/generator.py
+++ celebA/generator.py
@@ -1,9 +1,11 @@
 from keras.layers import (
     Dense,
     Conv2D,
-    UpSampling2D,
+    Conv2DTranspose,
+    BatchNormalization,
     Reshape,
-    Activation
+    Activation,
+    LeakyReLU
 )
 from keras.models import Sequential
 from keras import backend as K
@@ -12,19 +14,55 @@ from keras import backend as K
 def generator_mnist(input_dim, is_plot=False):
     print("Building Generator ...   ", end="")
     model = Sequential(name="generator")
-    model.add(Dense(input_dim=input_dim, units=(128 * 7 * 7)))
-    model.add(Activation('relu'))
+    model.add(Dense(input_dim=input_dim, units=(256 * 10 * 8)))
+    model.add(LeakyReLU(0.2))
 
     if K.image_dim_ordering() == 'th':
-        model.add(Reshape((128, 7, 7)))
+        model.add(Reshape((256, 10, 8)))
     else:
-        model.add(Reshape((7, 7, 128)))
+        model.add(Reshape((10, 8, 256)))
 
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Conv2D(64, (5, 5), padding='same'))
-    model.add(Activation('relu'))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Conv2D(1, (5, 5), padding='same'))
+    # (10, 8, 256) to (20, 16, 128)
+    model.add(Conv2DTranspose(256, (3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(256, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(128, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+
+    # (20, 16, 128) to (40, 32, 64)
+    model.add(Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(128, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+
+    # (40, 32, 64) to (80, 64, 32)
+    model.add(Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+
+    # (80, 64, 32) to (160, 128, 3)
+    model.add(Conv2DTranspose(32, (3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(3, (3, 3), padding='same'))
     model.add(Activation('tanh'))
 
     if is_plot:
